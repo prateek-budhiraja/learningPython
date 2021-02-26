57cf6a2b49990a71e07f580c694fd9d4a4b2b535
Prateek <prateekbudhiraja14@gmail.com>
2021-02-26 11:57:50 +0530

    initial commit

---
+++ intro/intro0.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+#comments
+
+'''
+this is not comments
+these are strings
+'''
+
+# print("Hello world!")
+
+# varname="hello"
+
+# name=input('Enter your name: ')
+# a=complex(name)
+
+# print('type of name', type(name), 'type of a', type(a))
+# print(a)
+
+# float()
+# str()
+# long()
+# complex()
+
+
+
+
+
+a=10
+b=20
+print(a,b)
+a,b=b,a
+print(a,b)
